9e1710f4e70f2abd61bbdd9e644c09973f11e8fc
Dinith Wannigama <dinith1@github.com>
2018-11-20 22:45:57 +1300

    swagger

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ReadMyLife.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ReadMyLife
 {
@@ -31,6 +32,13 @@ namespace ReadMyLife
 
             services.AddDbContext<ReadMyLifeContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("ReadMyLifeContext")));
+
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MemeBank", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +55,18 @@ namespace ReadMyLife
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
         }
     }
 }
